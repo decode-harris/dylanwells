1aba916211fe3fa5e5e3c4a001166766935346cf
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-03-25 03:25:35 +1100

    client feedback
    aligned existing content to clients specifications

--- src/js/index.js
+++ src/js/index.js
@@ -1,239 +1,131 @@
-const thouse_render = document.querySelector('#thouse_render');
-console.log(thouse_render);
+// const thouse_render = document.querySelector('#thouse_render');
+// console.log(thouse_render);
 
-const workingDrawing = document.querySelectorAll('.working-drawing');
+// const workingDrawing = document.querySelectorAll('.working-drawing');
 
-// console.log(project_thouse);
-
-
-// everist center render selectors
-const everist_render_01 = document.querySelector('#everist_render_01');
-const everist_render_02 = document.querySelector('#everist_render_02');
-
-const project_everist = document.querySelector('#project_everist');
-const everist_containers = document.querySelectorAll('#project_everist .working-drawing-container');
-
-console.log('everist containers : ' + everist_containers);
-
-const images = document.querySelectorAll('img');
-console.log(images);
+// // console.log(project_thouse);
 
 
-const thouse_images = document.querySelectorAll('#project_thouse img');
-const thouse_containers = document.querySelectorAll('#project_thouse .working-drawing-container');
+// // everist center render selectors
+// const everist_render_01 = document.querySelector('#everist_render_01');
+// const everist_render_02 = document.querySelector('#everist_render_02');
+
+// const project_everist = document.querySelector('#project_everist');
+// const everist_containers = document.querySelectorAll('#project_everist .working-drawing-container');
+
+// console.log('everist containers : ' + everist_containers);
+
+// const images = document.querySelectorAll('img');
+// console.log(images);
 
 
-const praxis_images = document.querySelectorAll('#project_praxis img');
-const praxis_containers = document.querySelectorAll('#project_praxis .working-drawing-container');
+// const thouse_images = document.querySelectorAll('#project_thouse img');
+// const thouse_containers = document.querySelectorAll('#project_thouse .working-drawing-container');
 
-const section_x = document.querySelectorAll('.section-x');
 
-let icons = document.querySelectorAll('.icon');
+// const praxis_images = document.querySelectorAll('#project_praxis img');
+// const praxis_containers = document.querySelectorAll('#project_praxis .working-drawing-container');
 
-let hidden = document.querySelectorAll('.text-hidden');
+// const section_x = document.querySelectorAll('.section-x');
+
+// let icons = document.querySelectorAll('.icon');
 
 
 
 
-// // function [ mql ] : media query listener
-window.onload = mql = (e)=> {
+
+
+// // // function [ mql ] : media query listener
+// window.onload = mql = (e)=> {
     
-    // media query [ mq ] variable for devices greater than 700px
-    let mq = window.matchMedia('(min-width: 700px)');
+//     // media query [ mq ] variable for devices greater than 700px
+//     let mq = window.matchMedia('(min-width: 700px)');
 
-    // assign event [ e ] to match media [ mq ] value
-    e = mq;
+//     // assign event [ e ] to match media [ mq ] value
+//     e = mq;
 
-    // validate if page matches media query [ mq ]
-    if (e.matches) {
+//     // validate if page matches media query [ mq ]
+//     if (e.matches) {
 
-        hidden.forEach(element => {
-            element.style.display = 'flex';
-        });
         
-        moreEnviron.style.display = 'none';
-        moreThouse.style.display = 'none';
-        moreBath.style.display = 'none';
-        moreEverist.style.display = 'none';
-        moreFacility.style.display = 'none';
-        moreMalt.style.display = 'none';
         
-        // thouse_render.setAttribute('src', 'src/assets/images/T_House/render-desktop.jpg');
+//         // // moreEnviron.style.display = 'none';
+//         // // moreThouse.style.display = 'none';
+//         // // moreBath.style.display = 'none';
+//         // // moreEverist.style.display = 'none';
+//         // // moreFacility.style.display = 'none';
+//         // // moreMalt.style.display = 'none';
         
-        thouse_containers.forEach(element => {
+//         // // thouse_render.setAttribute('src', 'src/assets/images/T_House/render-desktop.jpg');
+        
+//         // thouse_containers.forEach(element => {
 
-            // remove global styles from image element for specific width properties
-            element.removeAttribute('style', 'max-width: 100vw, max-height: 100vh');
-            element.setAttribute('style', 'min-width: 100vw');
+//         //     // remove global styles from image element for specific width properties
+//         //     element.removeAttribute('style', 'max-width: 100vw, max-height: 100vh');
+//         //     element.setAttribute('style', 'min-width: 100vw');
 
-            // test
-            console.log('thouse styles applied to mobile');
+//         //     // test
+//         //     console.log('thouse styles applied to mobile');
             
-        });
+//         // });
 
-        everist_containers.forEach(element => {
+//         // everist_containers.forEach(element => {
 
-            // remove global styles from image element for specific width properties
-            element.removeAttribute('style', 'max-width: 100vw, max-height: 100vh');
-            element.setAttribute('style', 'min-width: 100vw');
+//         //     // remove global styles from image element for specific width properties
+//         //     element.removeAttribute('style', 'max-width: 100vw, max-height: 100vh');
+//         //     element.setAttribute('style', 'min-width: 100vw');
 
-            // test
-            console.log('everist styles applied to mobile');
+//         //     // test
+//         //     console.log('everist styles applied to mobile');
             
-        });
+//         // });
         
-        images.forEach(element => {
+//         images.forEach(element => {
 
-            // remove global styles from image element for specific width properties
-            element.removeAttribute('style', 'max-width: 100vw, max-height: 100vh');
+//             // remove global styles from image element for specific width properties
+//             element.removeAttribute('style', 'max-width: 100vw, max-height: 100vh');
             
-            // test
-            console.log('images removed from mobile');
+//             // test
+//             console.log('images removed from mobile');
             
-        });
-        icons.forEach(element => {
-            element.setAttribute('style', 'max-width: 100px; max-height: 100px');
-        });
+//         });
+//         icons.forEach(element => {
+//             element.setAttribute('style', 'max-width: 100px; max-height: 100px');
+//         });
         
-        // test image
-        // console.log(thouse_render.src);
+//         // test image
+//         // console.log(thouse_render.src);
 
-        // test media query
-        console.log('[ mql ] : desktop media query');
+//         // test media query
+//         console.log('[ mql ] : desktop media query');
 
-    }
-    // default functions for switching to non matched media query [ mobile ]
-    else {
-
-        
-        thouse_render.setAttribute('src', 'src/assets/images/T_House/render-mobile.jpg');
-
-        // test image
-        console.log(thouse_render.src);
-
-        icons.forEach(element => {
-            element.setAttribute('style', 'max-width: 100px; max-height: 100px');
-        });
+//     }
+//     // default functions for switching to non matched media query [ mobile ]
+//     else {
 
         
+//         // thouse_render.setAttribute('src', 'src/assets/images/T_House/render-mobile.jpg');
+
+//         // test image
+//         // console.log(thouse_render.src);
+
+//         icons.forEach(element => {
+//             element.setAttribute('style', 'max-width: 100px; max-height: 100px');
+//         });
+
         
-        // test
-        console.log('[ mql ] : mobile media query');
+        
+//         // test
+//         console.log('[ mql ] : mobile media query');
 
-    }
+//     }
 
-    // event [ mq ] listener, run mql function on [ mq ] change
-    mq.addEventListener('change', mql);
+//     // event [ mq ] listener, run mql function on [ mq ] change
+//     mq.addEventListener('change', mql);
     
-};
-
-// element [ more-environment btn ]
-let moreEnviron = document.querySelector('#more-environment');
-let environText = document.querySelector('#more-environment-text');
-moreEnviron.style.display = 'flex';
+// };
 
 
-// event [ more-environment btn ]
-moreEnviron.addEventListener('click', ()=> {
-
-    if (environText.style.display != 'flex') {
-        environText.style.display = 'flex';
-        moreEnviron.innerHTML = 'less';
-    }
-    else {
-        environText.style.display = 'none';
-        moreEnviron.innerHTML = 'more';
-    }
-
-});
-
-// element [ more-facility btn ]
-let moreFacility = document.querySelector('#more-facility');
-let facilityText = document.querySelector('#more-facility-text');
-
-moreFacility.addEventListener('click', ()=> {
-
-    if (facilityText.style.display != 'flex') {
-        facilityText.style.display = 'flex';
-        moreFacility.innerHTML = 'less';
-    }
-    else {
-        facilityText.style.display = 'none';
-        moreFacility.innerHTML = 'more';
-    }
-
-});
-
-// element [ more-malt btn ]
-let moreMalt = document.querySelector('#more-malt');
-let maltText = document.querySelector('#more-malt-text');
-
-moreMalt.addEventListener('click', ()=> {
-
-    if (maltText.style.display != 'flex') {
-        maltText.style.display = 'flex';
-        moreMalt.innerHTML = 'less';
-    }
-    else {
-        maltText.style.display = 'none';
-        moreMalt.innerHTML = 'more';
-    }
-
-});
-
-// element [ more-everist btn ]
-let moreEverist = document.querySelector('#more-everist');
-let everistText = document.querySelector('#more-everist-text');
-
-moreEverist.addEventListener('click', ()=> {
-
-    if (everistText.style.display != 'flex') {
-        everistText.style.display = 'flex';
-        moreEverist.innerHTML = 'less';
-    }
-    else {
-        everistText.style.display = 'none';
-        moreEverist.innerHTML = 'more';
-    }
-
-});
-
-
-// element [ more-thouse btn ]
-let moreThouse = document.querySelector('#more-thouse');
-let thouseText = document.querySelector('#more-thouse-text');
-
-// event [ more-bath btn ]
-moreThouse.addEventListener('click', ()=> {
-
-    if (thouseText.style.display != 'flex') {
-        thouseText.style.display = 'flex';
-        moreThouse.innerHTML = 'less';
-    }
-    else {
-        thouseText.style.display = 'none';
-        moreThouse.innerHTML = 'more';
-    }
-
-});
-
-// element [ more-bath btn ]
-let moreBath = document.querySelector('#more-bath');
-let bathText = document.querySelector('#more-bath-text');
-
-// event [ more-bath btn ]
-moreBath.addEventListener('click', ()=> {
-
-    if (bathText.style.display != 'flex') {
-        bathText.style.display = 'flex';
-        moreBath.innerHTML = 'less';
-    }
-    else {
-        bathText.style.display = 'none';
-        moreBath.innerHTML = 'more';
-    }
-
-});
 
 // element [ down btn ]
 const down = document.querySelector('.down');
@@ -254,15 +146,15 @@ down.addEventListener('click', ()=> {
 
 
 
-const email = document.querySelector('#email');
+// const email = document.querySelector('#email');
 
-email.addEventListener('click', ()=> {
+// email.addEventListener('click', ()=> {
 
-    email.setAttribute('src', 'mailto:dylanwells@hotmail.com');
+//     email.setAttribute('src', 'mailto:dylanwells@hotmail.com');
 
-    console.log('email ev ' + email.src);
+//     console.log('email ev ' + email.src);
 
-});
+// });
 
 
 // function touch2Mouse(e)
---
+++ src/js/nav.js
@@ -0,0 +1,232 @@
+/*
+    state listener function
+
+    properties :
+
+        listens for state changes between mobile and desktop
+
+        apply mob || des functions as per device module
+
+
+*/
+
+// navigation component selector
+const nav = document.querySelector('.nav');
+// navigation link selector all
+const links = document.querySelectorAll('.link');
+// mobile menu button selector
+const menu = document.querySelector('.menu');
+// mobile exit button selector
+const exit = document.querySelector('.exit');
+
+// function [ stateChange ]
+function stateChange(e) {
+    
+    if (e.matches) { // If media query matches
+
+        // apply default mobile display props
+        // nav.style.display = 'none';
+
+        // change nav width to active [ 100% ] 
+        nav.style.width = '0';
+        exit.style.display = 'none';
+
+        // position variable [ undefined ]
+        
+        
+        // event [ window ] : scroll position listener
+        window.addEventListener('scroll', ()=> {
+            let pos;    
+            
+
+            // set a min section height for comparison
+            let sectionHeight = '100vh';
+            
+            // if scroll position is less than section height
+            if (window.scrollY < sectionHeight) {
+                
+                // get current position
+                pos = window.scrollY;
+                
+                // assign nav position prop to top of header [ native placement ]
+                nav.style.top = '0';
+        
+                // test 
+                console.log('[ window IF ] :' + pos + 'px');
+            }
+            else {
+
+                // get current position
+                pos = window.scrollY;
+
+                // header [ menu ] button position props
+                menu.style.top = '10px';
+                menu.style.right = '10px';
+
+                // nav component props
+                nav.style.top = pos;
+                nav.style.zIndex = '400';
+
+                // test
+                // console.log('current position : ' + pos);
+            
+            }
+        
+        }); // end [ window ] : scroll position listener
+
+        // const links = document.querySelectorAll('.link');
+        links.forEach(element => {
+
+            element.addEventListener('click', (e)=> {
+                // prevent default link behavior
+                // e.preventDefault();
+
+                if (nav.style.width != '0') {
+                    nav.style.width = '0';
+                }
+                if (exit.style.display != 'none') {
+                    exit.style.display = 'none';
+
+                    menu.style.display = 'flex';
+                }
+            });
+            
+        });
+        
+
+            
+        // event [ menu btn ] mobile
+        menu.addEventListener('click', ()=> {
+
+            exit.style.display = 'flex';
+            menu.style.display = 'none';
+            
+            // validate nav display props
+            if (nav.style.width != '100%') {
+                
+                // apply mobile nav display props
+                // nav.style.display = 'flex';
+
+                // change nav width to active [ 100% ] 
+                nav.style.width = '70%';
+
+                // get current position
+                pos = window.scrollY;
+
+                // nav component props
+                nav.style.top = pos + 'px';
+                nav.style.zIndex = '200';
+
+                // let navigation = document.querySelector('.navigation');
+                // navigation.style.zIndex = '400';
+
+                // set mobile nav to active
+                // nav.setAttribute('class', 'active');
+
+                // icon.classList = 'fas fa-times';
+                
+                // overlap menu btn with exit btn
+                exit.style.zIndex = 500;
+            }
+            
+            // menu.style.background = 'rgba(20, 20, 20, 0.822)';
+            
+            
+            // if (nav.style.width != '100%') {
+                
+            //     // change nav width to active [ 100% ] 
+            //     nav.style.width = '100%';
+            // }
+            // else {
+            
+            //     // change icon class list to [ bars ] menu
+            //     icon.classList = 'fas fa-bars';
+
+            //     // change nav width back to default [ 0% ]
+            //     nav.style.width = '0%';
+            
+            // }
+            
+        });
+
+        // mobile navigation exit event
+        exit.addEventListener('click', ()=> {
+
+            menu.style.display = 'flex';
+            exit.style.display = 'none';
+
+            if (nav.style.display != 'none') {
+                // nav.style.display = 'none';
+
+                nav.style.width = '0%';
+                // nav.setAttribute('class', 'nav');
+
+                // overlap menu btn with exit btn
+                exit.style.zIndex = 0;
+
+                // change icon class list to [ bars ] menu
+                // icon.classList = 'fas fa-bars';
+                
+            }
+
+            
+
+            // test
+            console.log('exit btn : none');
+            
+            exit.style.display = 'none';
+        });
+
+        // media query test element
+        // document.body.style.backgroundColor = "yellow";
+    }
+    else {
+
+        // apply default desktop display props
+        nav.style.display = 'flex';
+        nav.style.width = '100%';
+        // nav.style.width = '15vw';
+        // exit.style.display = 'none';
+
+        exit.style.display = 'none';
+
+        let hidden = document.querySelectorAll('.text-hidden');
+        hidden.forEach(element => {
+            element.style.display = 'flex';
+        });
+
+        // // apply default desktop class name
+        // nav.setAttribute('class', 'nav');
+        
+        // for each loop [ links ]
+        links.forEach(element => {
+            
+            element.addEventListener('click', ()=> {
+                // validate nav display type
+                if (nav.style.display !='flex') {
+                    nav.style.display = 'flex';
+
+                    menu.style.display = 'flex';
+                    exit.style.display = 'none';
+                }
+
+                
+
+                console.log('click');
+
+            });
+        
+        });
+
+        // // media query test element
+        // document.body.style.backgroundColor = "pink";
+    }
+
+};
+  
+//   mobile media query
+var mob = window.matchMedia("(max-width: 700px)"); // mobile match media query [ 700 px ]
+  
+stateChange(mob); // Call listener function at run time
+
+mob.addEventListener('change', stateChange); // Attach listener function on state changes
