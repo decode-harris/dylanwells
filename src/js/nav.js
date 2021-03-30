45b8118430ffc8f593b67e4306bcc9d29a567051
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-03-30 18:59:05 +1100

    removed draft styles

--- src/js/nav.js
+++ src/js/nav.js
@@ -24,21 +24,15 @@ function stateChange(e) {
     
     if (e.matches) { // If media query matches
 
-        // apply default mobile display props
-        // nav.style.display = 'none';
-
         // change nav width to active [ 100% ] 
         nav.style.width = '0';
         exit.style.display = 'none';
 
-        // position variable [ undefined ]
-        
-        
         // event [ window ] : scroll position listener
         window.addEventListener('scroll', ()=> {
+            // position variable
             let pos;    
             
-
             // set a min section height for comparison
             let sectionHeight = '100vh';
             
@@ -66,9 +60,6 @@ function stateChange(e) {
                 // nav component props
                 nav.style.top = pos;
                 nav.style.zIndex = '400';
-
-                // test
-                // console.log('current position : ' + pos);
             
             }
         
@@ -93,8 +84,6 @@ function stateChange(e) {
             
         });
         
-
-            
         // event [ menu btn ] mobile
         menu.addEventListener('click', ()=> {
 
@@ -104,9 +93,6 @@ function stateChange(e) {
             // validate nav display props
             if (nav.style.width != '100%') {
                 
-                // apply mobile nav display props
-                // nav.style.display = 'flex';
-
                 // change nav width to active [ 100% ] 
                 nav.style.width = '70%';
 
@@ -117,44 +103,20 @@ function stateChange(e) {
                 nav.style.top = pos + 'px';
                 nav.style.zIndex = '200';
 
-                // let navigation = document.querySelector('.navigation');
-                // navigation.style.zIndex = '400';
-
-                // set mobile nav to active
-                // nav.setAttribute('class', 'active');
-
-                // icon.classList = 'fas fa-times';
-                
                 // overlap menu btn with exit btn
                 exit.style.zIndex = 500;
             }
-            
-            // menu.style.background = 'rgba(20, 20, 20, 0.822)';
-            
-            
-            // if (nav.style.width != '100%') {
-                
-            //     // change nav width to active [ 100% ] 
-            //     nav.style.width = '100%';
-            // }
-            // else {
-            
-            //     // change icon class list to [ bars ] menu
-            //     icon.classList = 'fas fa-bars';
-
-            //     // change nav width back to default [ 0% ]
-            //     nav.style.width = '0%';
-            
-            // }
-            
+        
         });
 
         // mobile navigation exit event
         exit.addEventListener('click', ()=> {
 
+            // restore menu default styles
             menu.style.display = 'flex';
             exit.style.display = 'none';
 
+            // valdiate nav display props
             if (nav.style.display != 'none') {
                 // nav.style.display = 'none';
 
@@ -163,45 +125,29 @@ function stateChange(e) {
 
                 // overlap menu btn with exit btn
                 exit.style.zIndex = 0;
-
-                // change icon class list to [ bars ] menu
-                // icon.classList = 'fas fa-bars';
-                
+            
             }
-
             
-
-            // test
-            console.log('exit btn : none');
-            
+            // remove exit button
             exit.style.display = 'none';
         });
-
-        // media query test element
-        // document.body.style.backgroundColor = "yellow";
+    
     }
     else {
 
         // apply default desktop display props
         nav.style.display = 'flex';
         nav.style.width = '100%';
-        // nav.style.width = '15vw';
-        // exit.style.display = 'none';
-
+        
+        // remove exit button from view
         exit.style.display = 'none';
 
-        // let hidden = document.querySelectorAll('.text-hidden');
-        // hidden.forEach(element => {
-        //     element.style.display = 'flex';
-        // });
-
-        // // apply default desktop class name
-        // nav.setAttribute('class', 'nav');
         
         // for each loop [ links ]
         links.forEach(element => {
             
             element.addEventListener('click', ()=> {
+                
                 // validate nav display type
                 if (nav.style.display !='flex') {
                     nav.style.display = 'flex';
@@ -209,17 +155,11 @@ function stateChange(e) {
                     menu.style.display = 'flex';
                     exit.style.display = 'none';
                 }
-
-                
-
-                console.log('click');
-
+            
             });
         
         });
-
-        // // media query test element
-        // document.body.style.backgroundColor = "pink";
+    
     }
 
 };
